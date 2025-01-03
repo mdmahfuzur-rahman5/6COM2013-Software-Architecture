be5b3cf0dc17d7512475aff73461e79f52bacf9a
mdmahfuzur-rahman5 <mr22aed@herts.ac.uk>
2025-01-03 22:03:55 +0000

    Customer class added

---
+++ src/main/model/Customer.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Customer {
+    private String name;
+    private String parcelId;
+
+    public Customer(String name, String parcelId) {
+        this.name = name;
+        this.parcelId = parcelId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getParcelId() {
+        return parcelId;
+    }
+}
